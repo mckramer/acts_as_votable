fbcd4d024e1c7fe953781ec66780b67053d03961
Max Kramer <mckramer@gmail.com>
2014-07-01 01:14:27 -0400

    Fix rails4 issue when using protected_attributes gem

--- lib/acts_as_votable/vote.rb
+++ lib/acts_as_votable/vote.rb
@@ -5,7 +5,7 @@ module ActsAsVotable
 
     include Helpers::Words
 
-    if ::ActiveRecord::VERSION::MAJOR < 4
+    if defined?(ProtectedAttributes) || !defined?(ActionController::StrongParameters)
       attr_accessible :votable_id, :votable_type,
         :voter_id, :voter_type,
         :votable, :voter,
